786d56a3b2314176ffd6163ce6b08c790e56ce9d
firas <fruine6@gmail.com>
2022-03-04 23:20:54 +0100

    reply

--- src/views/Questions/QuestionPage.vue
+++ src/views/Questions/QuestionPage.vue
@@ -22,14 +22,14 @@
                 <img :src="'http://127.0.0.1:8000' + userprofile.imageU" alt="avatar" />
               </div>
             </template>
-            <h6>{{userprofile.firstname}} {{userprofile.lastname}}</h6>
-            <p class="meta-date-time">{{question.dateQ | formatDate}}</p>
-            <h4 class="mt-5">{{question.titleQ}}</h4>
+            <h6>{{ userprofile.firstname }} {{ userprofile.lastname }}</h6>
+            <p class="meta-date-time">{{ question.dateQ | formatDate }}</p>
+            <h4 class="mt-5">{{ question.titleQ }}</h4>
           </b-media>
         </div>
         <div class="widget-content">
-          <p class="ml-5">{{question.contentQ}}</p>
-          <div class="widget-content mb-5" v-if="question.imageQ!=null">
+          <h6 class="ml-5 mb-5">{{ question.contentQ }}</h6>
+          <div class="widget-content mb-5" v-if="question.imageQ != null">
             <img :src="'http://127.0.0.1:8000' + question.imageQ" class="rounded mx-auto d-block" style="max-width: 100%; height: auto" />
           </div>
           <div class="w-action">
@@ -48,7 +48,7 @@
               >
                 <path d="M14 9V5a3 3 0 0 0-3-3l-4 9v11h11.28a2 2 0 0 0 2-1.7l1.38-9a2 2 0 0 0-2-2.3zM7 22H4a2 2 0 0 1-2-2v-7a2 2 0 0 1 2-2h3"></path>
               </svg>
-              <span>{{question.nblikes}} Likes</span>
+              <span>{{ question.nblikes }} Likes</span>
             </div>
             <div class="card-like mr-4">
               <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" width="24" height="24" preserveAspectRatio="xMidYMid meet" viewBox="0 0 16 16">
@@ -72,101 +72,110 @@
                     <path fill="currentColor" d="m21.558 3.592l-1.15-1.15a1.49 1.49 0 0 0-2.12 0L13 7.731V11h3.27l5.288-5.288a1.49 1.49 0 0 0 0-2.12ZM15.579 9.45h-1.03V8.42L18 4.973l1.03 1.03Z" />
                     <path fill="currentColor" d="M19 19H5V5h6V3H5a2.006 2.006 0 0 0-2 2v14a2.006 2.006 0 0 0 2 2h14a2.006 2.006 0 0 0 2-2v-6h-2Z" />
                   </svg>
-                  <b-form-textarea type="text" class="mb-2" placeholder="Enter your reply here"></b-form-textarea>
+                  <b-form-textarea type="text" v-model="replies.contentR" class="mb-2" placeholder="Enter your reply content here"></b-form-textarea>
+                  <div class="mt-4 mb-5">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="mb-1" aria-hidden="true" role="img" width="24" height="24" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24">
+                      <g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
+                        <path d="M2 6a4 4 0 0 1 4-4h12a4 4 0 0 1 4 4v12a4 4 0 0 1-4 4H6a4 4 0 0 1-4-4V6Z" />
+                        <circle cx="8.5" cy="8.5" r="2.5" />
+                        <path d="M14.526 12.621L6 22h12.133A3.867 3.867 0 0 0 22 18.133V18c0-.466-.175-.645-.49-.99l-4.03-4.395a2 2 0 0 0-2.954.006Z" />
+                      </g>
+                    </svg>
+                    <b-file @change="onFileChanged"></b-file>
+                  </div>
                 </div>
-                <b-button variant="primary" block class="mt-2 mb-2" @click="$bvModal.hide('exampleModalCenter')">Submit</b-button>
+                <b-button variant="primary" block class="mt-2 mb-2" @click="reply">Submit</b-button>
               </form>
             </b-modal>
           </div>
           <div id="mediaObjectNotationIcon" class="col-lg-12 layout-spacing mt-5">
             <div class="panel-heading">
-              
-              <div v-if="question.nbrep!=0" class="row">
-                <h5 class="mt-4 ml-4">Answers</h5>
+              <div v-if="question.nbrep != 0" class="row">
+                <h5 class="mt-4 ml-4">{{ question.nbrep }} Answers</h5>
                 <div class="panel-body mb-3 pill-justify-right">
                   <b-tabs pills nav-class=" justify-content-end mb-5">
-                    <b-tab  title="Default" active>
+                    <b-tab title="Default" active>
                       <div v-for="rep in Replies" :key="rep.id">
-                      <div v-if="rep.questionRep==question.id" class="panel-body notation-text-icon">
-                        <b-media>
-                          <div v-for="u in Userprofiles" :key="u.id">
-                          <div v-if="u.id==rep.userprofileRep">
-                            <div class="float-left">
-                            <img class="rounded mr-2" :src="'http://127.0.0.1:8000' + u.imageU" width="40px" alt="pic1" />
-                            </div>
-                          <h6 class="">{{u.firstname}} {{u.lastname}}</h6>
-                          </div>
-                          </div>
-                          <p class="meta-date-time media-text mb-4">{{rep.dateR | formatDate}}</p>
-                          <p class="media-text"> 
-                            {{rep.contentR}}
-                          </p>
-                          <div class="widget-content mb-4">
-                            <img :src="'http://127.0.0.1:8000' + rep.imageR" class="rounded mx-auto d-block" style="max-width: 100%; height: auto" />
-                          </div>
-                          <div class="media-notation float-right">
-                            <a href="javascript:void(0);" class="mr-2"
-                              ><svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                width="22"
-                                height="22"
-                                viewBox="0 0 24 30"
-                                fill="none"
-                                stroke="currentColor"
-                                stroke-width="2"
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-                                class="feather feather-thumbs-up"
-                              >
-                                <path d="M14 9V5a3 3 0 0 0-3-3l-4 9v11h11.28a2 2 0 0 0 2-1.7l1.38-9a2 2 0 0 0-2-2.3zM7 22H4a2 2 0 0 1-2-2v-7a2 2 0 0 1 2-2h3"></path>
-                              </svg>
-                              {{ rep.nblikesR }} likes
-                            </a>
-                            <a href="javascript:void(0);" class="mr-2"
-                              ><svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" width="20" height="20" preserveAspectRatio="xMidYMid meet" viewBox="0 0 1792 1536">
-                                <path
-                                  fill="currentColor"
-                                  d="M896 128q-204 0-381.5 69.5T232.5 385T128 640q0 112 71.5 213.5T401 1029l87 50l-27 96q-24 91-70 172q152-63 275-171l43-38l57 6q69 8 130 8q204 0 381.5-69.5t282-187.5T1664 640t-104.5-255t-282-187.5T896 128zm896 512q0 174-120 321.5t-326 233t-450 85.5q-70 0-145-8q-198 175-460 242q-49 14-114 22h-5q-15 0-27-10.5t-16-27.5v-1q-3-4-.5-12t2-10t4.5-9.5l6-9l7-8.5l8-9q7-8 31-34.5t34.5-38t31-39.5t32.5-51t27-59t26-76q-157-89-247.5-220T0 640q0-174 120-321.5t326-233T896 0t450 85.5t326 233T1792 640z"
-                                />
-                              </svg>
-                              {{rep.nbCommentR}} comments
-                            </a>
-                            <a href="javascript:void(0);" class=""
-                              ><svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                width="20"
-                                height="20"
-                                viewBox="0 0 24 24"
-                                fill="none"
-                                stroke="currentColor"
-                                stroke-width="2"
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-                                class="feather feather-message-square"
-                              >
-                                <path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"></path>
-                              </svg>
-                              <span v-b-modal.registerModal1>Add a comment</span>
-                            </a>
-                            <!-- Comment Modal -->
-                            <b-modal id="registerModal1" hide-footer title="Add Comment" title-tag="h4" modal-class="register-modal" footer-class="justify-content-center">
-                              <form class="mt-0">
-                                <div class="form-group">
-                                  <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" width="24" height="24" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24">
-                                    <path
-                                      fill="currentColor"
-                                      d="m21.558 3.592l-1.15-1.15a1.49 1.49 0 0 0-2.12 0L13 7.731V11h3.27l5.288-5.288a1.49 1.49 0 0 0 0-2.12ZM15.579 9.45h-1.03V8.42L18 4.973l1.03 1.03Z"
-                                    />
-                                    <path fill="currentColor" d="M19 19H5V5h6V3H5a2.006 2.006 0 0 0-2 2v14a2.006 2.006 0 0 0 2 2h14a2.006 2.006 0 0 0 2-2v-6h-2Z" />
-                                  </svg>
-                                  <b-form-textarea type="text" class="mb-2" placeholder="Enter your comment here"></b-form-textarea>
+                        <div v-if="rep.questionRep == question.id" class="panel-body notation-text-icon">
+                          <b-media>
+                            <div v-for="u in Userprofiles" :key="u.id">
+                              <div v-if="u.id == rep.userprofileRep">
+                                <div class="float-left">
+                                  <img class="rounded mr-2" :src="'http://127.0.0.1:8000' + u.imageU" width="40px" alt="pic1" />
                                 </div>
-                                <b-button variant="primary" block class="mt-2 mb-2" @click="$bvModal.hide('registerModal1')">Submit</b-button>
-                              </form>
-                            </b-modal>
-                          </div>
-                        </b-media>
-                      </div>
+                                <h6 class="">{{ u.firstname }} {{ u.lastname }}</h6>
+                              </div>
+                            </div>
+                            <p class="meta-date-time media-text mb-4">{{ rep.dateR | formatDate }}</p>
+                            <h5 class="media-text ml-5 mb-5">
+                              {{ rep.contentR }}
+                            </h5>
+                            <div v-if="rep.imageR != null" class="widget-content mb-4">
+                              <img :src="'http://127.0.0.1:8000' + rep.imageR" class="rounded mx-auto d-block" style="max-width: 100%; height: auto" />
+                            </div>
+                            <div class="media-notation float-right">
+                              <a href="javascript:void(0);" class="mr-2"
+                                ><svg
+                                  xmlns="http://www.w3.org/2000/svg"
+                                  width="22"
+                                  height="22"
+                                  viewBox="0 0 24 30"
+                                  fill="none"
+                                  stroke="currentColor"
+                                  stroke-width="2"
+                                  stroke-linecap="round"
+                                  stroke-linejoin="round"
+                                  class="feather feather-thumbs-up"
+                                >
+                                  <path d="M14 9V5a3 3 0 0 0-3-3l-4 9v11h11.28a2 2 0 0 0 2-1.7l1.38-9a2 2 0 0 0-2-2.3zM7 22H4a2 2 0 0 1-2-2v-7a2 2 0 0 1 2-2h3"></path>
+                                </svg>
+                                {{ rep.nblikesR }} likes
+                              </a>
+                              <a href="javascript:void(0);" class="mr-2"
+                                ><svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" width="20" height="20" preserveAspectRatio="xMidYMid meet" viewBox="0 0 1792 1536">
+                                  <path
+                                    fill="currentColor"
+                                    d="M896 128q-204 0-381.5 69.5T232.5 385T128 640q0 112 71.5 213.5T401 1029l87 50l-27 96q-24 91-70 172q152-63 275-171l43-38l57 6q69 8 130 8q204 0 381.5-69.5t282-187.5T1664 640t-104.5-255t-282-187.5T896 128zm896 512q0 174-120 321.5t-326 233t-450 85.5q-70 0-145-8q-198 175-460 242q-49 14-114 22h-5q-15 0-27-10.5t-16-27.5v-1q-3-4-.5-12t2-10t4.5-9.5l6-9l7-8.5l8-9q7-8 31-34.5t34.5-38t31-39.5t32.5-51t27-59t26-76q-157-89-247.5-220T0 640q0-174 120-321.5t326-233T896 0t450 85.5t326 233T1792 640z"
+                                  />
+                                </svg>
+                                {{ rep.nbCommentR }} comments
+                              </a>
+                              <a href="javascript:void(0);" class=""
+                                ><svg
+                                  xmlns="http://www.w3.org/2000/svg"
+                                  width="20"
+                                  height="20"
+                                  viewBox="0 0 24 24"
+                                  fill="none"
+                                  stroke="currentColor"
+                                  stroke-width="2"
+                                  stroke-linecap="round"
+                                  stroke-linejoin="round"
+                                  class="feather feather-message-square"
+                                >
+                                  <path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"></path>
+                                </svg>
+                                <span v-b-modal.registerModal1>Add a comment</span>
+                              </a>
+                              <!-- Comment Modal -->
+                              <b-modal id="registerModal1" hide-footer title="Add Comment" title-tag="h4" modal-class="register-modal" footer-class="justify-content-center">
+                                <form class="mt-0">
+                                  <div class="form-group">
+                                    <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" width="24" height="24" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24">
+                                      <path
+                                        fill="currentColor"
+                                        d="m21.558 3.592l-1.15-1.15a1.49 1.49 0 0 0-2.12 0L13 7.731V11h3.27l5.288-5.288a1.49 1.49 0 0 0 0-2.12ZM15.579 9.45h-1.03V8.42L18 4.973l1.03 1.03Z"
+                                      />
+                                      <path fill="currentColor" d="M19 19H5V5h6V3H5a2.006 2.006 0 0 0-2 2v14a2.006 2.006 0 0 0 2 2h14a2.006 2.006 0 0 0 2-2v-6h-2Z" />
+                                    </svg>
+                                    <b-form-textarea type="text" class="mb-2" placeholder="Enter your comment here"></b-form-textarea>
+                                  </div>
+                                  <b-button variant="primary" block class="mt-2 mb-2" @click="$bvModal.hide('registerModal1')">Submit</b-button>
+                                </form>
+                              </b-modal>
+                            </div>
+                          </b-media>
+                        </div>
                       </div>
                     </b-tab>
                     <b-tab title="Newest">
@@ -419,28 +428,76 @@
 
 
 <script>
-
-
-import axios from 'axios'
+import axios from 'axios';
 
 import '@/assets/sass/widgets/widgets.scss';
 import { mapGetters, mapActions } from 'vuex';
 export default {
-  data(){
-    return{
-      question:[],
-      userprofile:[],
-    }
+  data() {
+    return {
+      question: [],
+      userprofile: [],
+      userprofileRep: [],
+      CurrentUserProfile:[],
+      replies: {
+        contentR: '',
+      },
+      image: null,
+    };
   },
   computed: {
     ...mapGetters({
       Questions: 'StateQuestions',
-      Replies:'StateReplies',
-      Userprofiles:'StateUserprofiles'
+      Replies: 'StateReplies',
+      Userprofiles: 'StateUserprofiles',
     }),
   },
   methods: {
-    ...mapActions(['GetQuestions','GetReplies','GetUserprofiles','CreateReply','CreateComment']),
+    ...mapActions(['GetQuestions', 'GetReplies', 'GetUserprofiles', 'CreateReply', 'CreateComment']),
+    onFileChanged(event) {
+      this.image = event.target.files[0];
+    },
+
+    async reply() {
+      try {
+        for (let u in this.Userprofiles) {
+          if (this.Userprofiles[u].userU == this.CurrentUser.id) {
+            this.userprofileRep = this.Userprofiles[u].id;
+            this.CurrentUserProfile = this.Userprofiles[u];
+          }
+        }
+
+        var formdata = new FormData();
+        if (this.image != null) {
+          formdata.append('imageR', this.image);
+        }
+        formdata.append('contentR', this.replies.contentR);
+        formdata.append('questionRep', this.$route.params.id);
+        formdata.append('userprofileRep', this.userprofileRep);
+        await this.CreateReply(formdata);
+        await axios.put('/question/question-update/' + this.$route.params.id + '/', {
+          nbrep: (this.question.nbrep += 1),
+        });
+        /*for(let r in this.Replies)
+        {
+          if(this.Replies[r].userprofileRep == this.form.userprofileRep && this.Replies[r].questionRep == this.form.questionRep )
+          {
+             this.replyNo = this.Replies[r]
+          }
+        }
+        if(this.question.userprofileQ != this.CurrentUserProfile.id)
+        await axios.post("/notification/notification-create/" , {
+          userprofileNo: this.question.userprofileQ, questionNo:this.$route.params.id, replyNo: this.repNo.id,contentNo: this.CurrentUserProfile.lastname+" "+this.CurrentUserProfile.firstname+" has responded to your question"
+        });*/
+        this.replies.contentR = '';
+        await axios.put(
+          "/userprofile/userprofile-update/" + this.userprofileRep + "/",
+          { nbreplies: (this.CurrentUserProfile.nbreplies += 1) }
+        );
+      } catch (error) {
+        throw 'Il ya un error!';
+      }
+    },
   },
   created() {
     //this.GetComments();
@@ -448,7 +505,6 @@ export default {
     this.GetReplies();
     //this.GetUsers();
     this.GetUserprofiles();
-    
 
     for (let u in this.Users) {
       if (this.Users[u].username == this.User) {
@@ -462,13 +518,12 @@ export default {
       }
     }*/
     axios
-      .get("/question/question-detail/" + this.$route.params.id + "/")
+      .get('/question/question-detail/' + this.$route.params.id + '/')
       .then((response) => {
         this.question = response.data;
-        axios.get("/userprofile/userprofile-detail/" + this.question.userprofileQ + "/")
-      .then((response) => {
-        this.userprofile = response.data;
-      })
+        axios.get('/userprofile/userprofile-detail/' + this.question.userprofileQ + '/').then((response) => {
+          this.userprofile = response.data;
+        });
       })
       .catch((err) => {
         console.log(err);
