e40ec10fc1b2b0e879c607540e445d94626c2b1f
mririi <wassim.mriri@gmail.com>
2022-03-19 20:40:50 +0100

    backend clean up

--- Backend/badge/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class BadgeConfig(AppConfig):
-    name = 'badge'
--- Backend/badge/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Badge
-
-class BadgeSerializer(serializers.ModelSerializer):
-	class Meta:
-		model = Badge
-		fields ='__all__'
