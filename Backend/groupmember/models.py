5d9cdfb676ed18e91a1fb17af8b49fca9a7a1d78
firas <fruine6@gmail.com>
2022-04-01 14:34:55 +0100

    Groups ....

--- Backend/groupmember/models.py
+++ Backend/groupmember/models.py
@@ -2,8 +2,9 @@ from django.db import models
 
 # Create your models here.
 class GroupMember(models.Model):
+  accepted = models.BooleanField(default=False)
   userprofileMem =models.ForeignKey('userprofile.Userprofile',on_delete=models.CASCADE,blank=True,null=True)
   groupMem =models.ForeignKey('group.Group',on_delete=models.CASCADE,blank=True,null=True)
 
   def __str__(self):
-    return self.groupMem
+    return self.groupMem.__str__()
--- src/App.vue
+++ src/App.vue
@@ -1,59 +1,58 @@
 <template>
-    <div :class="[$store.state.layout_style, $store.state.menu_style]">
-        <component v-bind:is="layout"></component>
-    </div>
+  <div :class="[$store.state.layout_style, $store.state.menu_style]">
+    <component v-bind:is="layout"></component>
+  </div>
 </template>
 <script>
-    // layouts
-    import appLayout from './layouts/app-layout.vue';
-    import authLayout from './layouts/auth-layout.vue';
-    import axios from 'axios';
-    import '@/assets/sass/app.scss';
+// layouts
+import appLayout from './layouts/app-layout.vue';
+import authLayout from './layouts/auth-layout.vue';
+import axios from 'axios';
+import '@/assets/sass/app.scss';
 import questionLayout from './layouts/question-layout.vue';
 import serviceLayout from './layouts/service-layout.vue';
 import dashboardLayout from './layouts/dashboard-layout.vue';
-import { mapGetters, mapActions } from "vuex";
+import { mapGetters, mapActions } from 'vuex';
 import groupLayout from './layouts/group-layout.vue';
-    export default {
-        metaInfo: {
-            title: 'Home',
-            titleTemplate: '%s | Car Geeks'
-        },
-        components: {
-            app: appLayout,
-            auth: authLayout,
-            question: questionLayout,
-            service: serviceLayout,
-            dashboard: dashboardLayout,
-            group: groupLayout
-        },
-        computed: {
-            layout() {
-                return this.$store.getters.layout;
-            },
-            ...mapGetters({
-      Userprofiles: "StateUserprofiles",})
-        },
-        data() {
-            return {};
-        },
-        created(){
-            this.GetUserprofiles();
-            for(let u in this.Userprofiles){
-                if(this.Userprofiles[u].nbquestions>=10 && this.Userprofiles[u].nbquestions<20){
-                    axios.put('/userprofile/userprofile-update/'+this.Userprofiles[u].id+'/',{badgeU:1})
-                }
-                else if (this.Userprofiles[u].nbquestions>=20 && this.Userprofiles[u].nbquestions<30)
-                {
-                    axios.put('/userprofile/userprofile-update/'+this.Userprofiles[u].id+'/',{badgeU:2})
-                }else if(this.Userprofiles[u].nbquestions>=30){
-                    axios.put('/userprofile/userprofile-update/'+this.Userprofiles[u].id+'/',{badgeU:3})
-                }
-            }
-        },
-        mounted() {},
-        methods: {
-            ...mapActions(["GetUserprofiles",])
-        }
-    };
+export default {
+  metaInfo: {
+    title: 'Home',
+    titleTemplate: '%s | Car Geeks',
+  },
+  components: {
+    app: appLayout,
+    auth: authLayout,
+    question: questionLayout,
+    service: serviceLayout,
+    dashboard: dashboardLayout,
+    group: groupLayout,
+  },
+  computed: {
+    layout() {
+      return this.$store.getters.layout;
+    },
+    ...mapGetters({
+      Userprofiles: 'StateUserprofiles',
+    }),
+  },
+  data() {
+    return {};
+  },
+  created() {
+    this.GetUserprofiles();
+    for (let u in this.Userprofiles) {
+      if (this.Userprofiles[u].nbquestions >= 10 && this.Userprofiles[u].nbquestions < 20) {
+        axios.put('/userprofile/userprofile-update/' + this.Userprofiles[u].id + '/', { badgeU: 1 });
+      } else if (this.Userprofiles[u].nbquestions >= 20 && this.Userprofiles[u].nbquestions < 30) {
+        axios.put('/userprofile/userprofile-update/' + this.Userprofiles[u].id + '/', { badgeU: 2 });
+      } else if (this.Userprofiles[u].nbquestions >= 30) {
+        axios.put('/userprofile/userprofile-update/' + this.Userprofiles[u].id + '/', { badgeU: 3 });
+      }
+    }
+  },
+  mounted() {},
+  methods: {
+    ...mapActions(['GetUserprofiles']),
+  },
+};
 </script>
