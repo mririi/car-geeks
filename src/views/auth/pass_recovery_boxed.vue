c15e50ab7caab15b1ff688df8f93663ca1762359
mririi <wassim.mriri@gmail.com>
2022-04-02 13:35:28 +0100

    recovery password delays

--- src/views/auth/pass_recovery_boxed.vue
+++ src/views/auth/pass_recovery_boxed.vue
@@ -28,12 +28,18 @@
                       <circle cx="12" cy="12" r="4"></circle>
                       <path d="M16 8v5a3 3 0 0 0 6 0v-1a10 10 0 1 0-3.92 7.94"></path>
                     </svg>
-                    <b-input type="email" v-model="email" placeholder="Email"></b-input>
+                    <b-input type="email"
+                     :class="[is_submit_form1 ? (email && email_validate(email) ? 'is-valid' : 'is-invalid') : '']"
+                     v-model="email" placeholder="Email"></b-input>
+                  <b-form-valid-feedback>Form looks good!</b-form-valid-feedback>
+                    <b-form-invalid-feedback :class="{ 'd-block': is_submit_form1 && !email_validate(email) }">Please fill a correct email !</b-form-invalid-feedback>
+                 
                   </div>
 
                   <div class="d-sm-flex justify-content-between">
                     <div class="field-wrapper">
-                      <b-button type="submit" variant="primary">Reset</b-button>
+                      <b-button v-show="disable" variant="primary" class="disabled">Sending the email</b-button>
+                      <b-button v-show="!disable" type="submit" variant="primary">Reset</b-button>
                     </div>
                   </div>
                 </div>
@@ -55,22 +61,35 @@ export default {
     return {
       email: '',
       result:[],
-      CurrentUser:[]
+      disable:false,
+      is_submit_form1:false,
     };
   },
-  metaInfo: { title: 'Password Recovery Boxed' },
+  metaInfo: { title: 'Password Recovery' },
   mounted() {},
   methods: {
-    ...mapActions(['SendEmail','GetUsers']),
-    submit() {
+    ...mapActions(['SendEmail']),
+    email_validate(email) {
+      const regexp = /^[\w.%+-]+@[\w.-]+\.[\w]{2,6}$/;
+      return regexp.test(email);
+    },
+    async submit() {
+      this.is_submit_form1 = true;
+      if (this.email) {
+      try{
+      this.disable=true
       let result=[]
-      axios.post('/api/password_reset/',{email:this.email}).then((res) => {
+      await axios.post('/api/password_reset/',{email:this.email}).then((res) => {
         result = res.data;
         if(result.status==="OK")
      {
+      this.disable=false
       this.$swal('Email Sent!','Please check your email !','success')
      }
-      })
+      })}catch(error){
+        this.disable=false
+        this.$swal('Something went wrong!','This email does not exist in our database !','error')
+      }}
     }}
    
 };
--- src/views/auth/pass_recovery_confirm.vue
+++ src/views/auth/pass_recovery_confirm.vue
@@ -9,9 +9,9 @@
               <p class="signup-link recovery">Enter your New Password !</p>
               <b-form class="text-left" @submit.prevent="submit">
                 <div class="form">
-                  <div id="email-field" class="field-wrapper input">
+                  <div id="password-field" class="field-wrapper input mb-2">
                     <div class="d-flex justify-content-between">
-                      <label for="email">EMAIL</label>
+                      <label for="password">PASSWORD</label>
                     </div>
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
@@ -23,17 +23,41 @@
                       stroke-width="2"
                       stroke-linecap="round"
                       stroke-linejoin="round"
-                      class="feather feather-at-sign"
+                      class="feather feather-lock"
                     >
-                      <circle cx="12" cy="12" r="4"></circle>
-                      <path d="M16 8v5a3 3 0 0 0 6 0v-1a10 10 0 1 0-3.92 7.94"></path>
+                      <rect x="3" y="11" width="18" height="11" rx="2" ry="2"></rect>
+                      <path d="M7 11V7a5 5 0 0 1 10 0v4"></path>
                     </svg>
-                    <b-input type="password" v-model="password" placeholder="Password"></b-input>
+                    <b-input
+                      :type="pwd_type"
+                      placeholder="Password"
+                      v-model="password"
+                      :class="[is_submit_form1 ? (password && password.length >= 8 ? 'is-valid' : 'is-invalid') : '']"
+                    ></b-input>
+                    <svg
+                      @click="set_pwd_type"
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      stroke-width="2"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      id="toggle-password"
+                      class="feather feather-eye"
+                    >
+                      <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
+                      <circle cx="12" cy="12" r="3"></circle>
+                    </svg>
+                    <b-form-invalid-feedback :class="{ 'd-block': is_submit_form1 && !password.length >= 8 }">Please enter a password longer than 8 characters !</b-form-invalid-feedback>
                   </div>
 
                   <div class="d-sm-flex justify-content-between">
                     <div class="field-wrapper">
-                      <b-button type="submit" variant="primary">Change</b-button>
+                      <b-button v-show="disable" variant="primary" class="disabled">Changing...</b-button>
+                      <b-button v-show="!disable" type="submit" variant="primary">Change</b-button>
                     </div>
                   </div>
                 </div>
@@ -55,27 +79,45 @@ export default {
     return {
       password: '',
       result:[],
+      pwd_type: 'password',
+      is_submit_form1: false,
+      valid:false,
+      disable:false,
     };
   },
   metaInfo: { title: 'Password Recovery Boxed' },
   mounted() {},
   methods: {
     ...mapActions(['SendEmail','GetUsers']),
-    submit() {
+    set_pwd_type() {
+      if (this.pwd_type == 'password') {
+        this.pwd_type = 'text';
+      } else {
+        this.pwd_type = 'password';
+      }
+    },
+    async submit() {
+      this.is_submit_form1 = true;
+      if (this.password.length>=8) {
+        try {
+          this.disable=true
       let result=[]
-      axios.post('/api/password_reset/confirm/',{password:this.password,token:this.$route.query.token}).then((res) => {
+      await axios.post('/api/password_reset/confirm/',{password:this.password,token:this.$route.query.token}).then((res) => {
         result = res.data;
-        console.log(result)
+        console.log(res.data)
         if(result.status==="OK")
      {
-      this.$router.push('/login')
+       this.disable=false
+      this.$router.push('/auth/login')
       this.$swal('Password Changed!','Please login with your new password !','success')
      }
       })
-    }},
-    created(){
-      console.log(this.$route.query.token)
+    } catch (error) {
+          this.disable=false
+          this.$swal('Something went wrong!','Your token is expired or Your password is either too commun, entirely numeric or too similar to the username !','error')
+        }
     }
-   
+    },
+  }
 };
 </script>
