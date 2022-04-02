c8796ff3f1949241e698919391367c227d38927b
mririi <wassim.mriri@gmail.com>
2022-04-03 00:48:32 +0100

    email unique

--- Backend/userapi/models.py
+++ Backend/userapi/models.py
@@ -5,7 +5,8 @@ from django.dispatch import receiver
 from django.urls import reverse
 from django_rest_passwordreset.signals import reset_password_token_created
 from django.core.mail import send_mail  
-
+from django.contrib.auth.models import User
+User._meta.get_field('email')._unique = True
 
 @receiver(reset_password_token_created)
 def password_reset_token_created(sender, instance, reset_password_token, *args, **kwargs):
