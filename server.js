cb0c0a0e1f8507c9edc44650d9cc4f099e540f37
mririi <wassim.mriri@gmail.com>
2022-04-04 15:13:39 +0100

    initial commit2

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express')
-const serveStatic = require('serve-static')
+var history = require('connect-history-api-fallback')
 const path = require('path')
+const serveStatic = require('serve-static')
+app.use(history({
+    // OPTIONAL: Includes more verbose logging
+    verbose: true
+}))
 const app = express()
 app.use('/',serveStatic(path.join(__dirname,'/dist')))
 const port = process.env.PORT || 8080
