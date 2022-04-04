607e7fcaaeefaf45806b38f579f095b53c3a97b1
mririi <wassim.mriri@gmail.com>
2022-04-04 18:37:20 +0100

    initial

--- server.js
+++ server.js
@@ -1,13 +1,7 @@
 const express = require('express')
 
 const path = require('path')
-
-const serveStatic = require('serve-static')
-
 const app = express()
-
 app.use("/", express.static(path.join(__dirname, "/dist")))
-
 const port = process.env.PORT || 8080
-
 app.listen(port)
