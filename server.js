07d98ce49c36d83500e2f1183bd8cdcf24819035
mririi <wassim.mriri@gmail.com>
2022-04-04 15:46:47 +0100

    initial commit

--- server.js
+++ server.js
@@ -1,10 +1,4 @@
 const express = require('express')
-var history = require('connect-history-api-fallback')
 const path = require('path')
-const serveStatic = require('serve-static')
-app.use(history({
-    // OPTIONAL: Includes more verbose logging
-    verbose: true
-}))
 const app = express()
 app.use('/',serveStatic(path.join(__dirname,'/dist')))
