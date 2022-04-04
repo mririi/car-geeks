178c7d83fab88c5f3c246a3bb6373ae2312189a4
mririi <wassim.mriri@gmail.com>
2022-04-04 19:10:55 +0100

    initial2

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ const app = express()
 
 app.use('/',serveStatic(path.join(__dirname,'/dist')))
 
+app.get(/.*/,function(req,res){
+    res.sendFile(path.join(__dirname, '/dist/index.html'))
+})
 const port = process.env.PORT || 8080
 
 app.listen(port)
