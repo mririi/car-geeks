0b36dd134ac4bca13c996e5bc5453adec7e6a9e1
mririi <wassim.mriri@gmail.com>
2022-04-23 16:02:50 +0100

    added chat to notif and fixed recovery password

--- userapi/models.py
+++ userapi/models.py
@@ -11,7 +11,7 @@ User._meta.get_field('email')._unique = True
 @receiver(reset_password_token_created)
 def password_reset_token_created(sender, instance, reset_password_token, *args, **kwargs):
 
-    email_plaintext_message = "This is the link to reset your password : http://localhost:8080/api/password_reset_confirm/?token={}".format(reset_password_token.key)
+    email_plaintext_message = "This is the link to reset your password : https://cargeeksjs.herokuapp.com/api/password_reset_confirm/?token={}".format(reset_password_token.key)
 
     send_mail(
         # title:
